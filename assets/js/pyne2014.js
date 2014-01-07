c7bc92d7394386d237a39cf0506912886b2a8f81
Helielson <helielson@jusbrasil.com.br>
2014-01-07 02:07:41 -0300

    Fixes selected menu js

--- assets/js/pyne2014.js
+++ assets/js/pyne2014.js
@@ -14,9 +14,14 @@ $(function() {
 
   pyne2014.scrollPage = function(articleToGo){
     var _this = this,
-        articleTopPosition = $('.' + articleToGo).offset().top;
+        articleOffset = $('.' + articleToGo).offset();
+
+    if (!articleOffset) {
+      return;
+    }
+
     $('body').stop().animate({
-      scrollTop : articleTopPosition - _this.HEADER_HEIGHT
+      scrollTop : articleOffset.top - _this.HEADER_HEIGHT
     }, 1000, function(){
     document.location.hash = articleToGo;
     $('a', '.navbar-nav').removeClass('active');
@@ -41,43 +46,6 @@ $(function() {
       var articleToGo = $(this).data('article');
       _this.scrollPage(articleToGo);
     });
-
-    var $window = $(window);
-    $window.scroll(function() {
-
-
-      var TOLERANCE = 200,
-          aboutTop = $('.about').offset().top,
-          speakersTop = $('.speakers').offset().top,
-          sponsorsTop = $('.sponsors').offset().top,
-          contactUsTop = $('.contact-us').offset().top,
-          windowTop = $window.scrollTop() + _this.HEADER_HEIGHT + TOLERANCE;
-
-      var $currentArticle;
-      if (windowTop < aboutTop) {
-        $currentArticle = null;
-      }
-      else if (windowTop < speakersTop) {
-        $currentArticle = $('a[data-article=about]');
-      }
-      else if (windowTop < sponsorsTop) {
-        $currentArticle = $('a[data-article=services]');
-      }
-      else if (windowTop < contactUsTop) {
-        $currentArticle = $('a[data-article=portfolio]');
-      }
-      else {
-        $currentArticle = $('a[data-article=contact-us]');
-      }
-      if ($currentArticle && !$currentArticle.hasClass('active')) {
-          $('a', '.navbar-nav').removeClass('active');
-          $currentArticle.addClass('active');
-      }
-      else if (!$currentArticle) {
-          $('a', '.navbar-nav').removeClass('active');
-      }
-    });
-
   };
 
   pyne2014.init();
